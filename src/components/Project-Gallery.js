41f61a6a511af2fb470b8d9252e2feb669a5a81f
Amdalat Jinadu <babi.jinadu@gmail.com>
2023-03-19 20:04:39 +0000

    added 6 projects to project.js

--- src/components/Project-Gallery.js
+++ src/components/Project-Gallery.js
@@ -1,15 +1,13 @@
-import roject from "./components/project.js"
+import Project from "./components/project.js"
+import React, {useState} from 'react';
 //passing my projects to the projectgallery function you can use useState (contains the description of the projects) and then that use usestate object have all the names of all your rpoject and the descriptions and all the requirements or my project and then reurn it into your div project gallery would contain all the css to style the gallery to render it on-screen as a gallery
 //inside project gallery div I will be passing in my project
 function projectGallery() {
     return (
         <div id="projectGallery">
-            //pass the project into the gallery//
-            
-        <img></img>
-        <h1>Amdalat Jinadu</h1>
-        <p>paragraph discussing my skills and experience.</p>
+         {/*pass the project into the gallery*/}
+         <Project projects={this.state.projects} ></Project>
         </div>
       );
 }
-export default home;
+export default projectGallery;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,39 @@
-project() {
+function Project(projects) {
     return (
     <div>
-    <h1 href=project.js link>project title</h1>
-    <img>from asset folder</img>
-    <link href=project,js link></link>
+     <div>  
+    <h1 href="https://amdalat1000000.github.io/The-Password-Generator-2/" link>Password Generator</h1>
+    <img src="link to image from in assets folder">a screenshot from the website that is uploaded to the asset folder</img>
+    <link href="https://github.com/Amdalat1000000/The-Password-Generator-2" link>Password Generator Github Repository</link>
     </div>
-    
+    <div>  
+    <h1 href="https://amdalat1000000.github.io/Code-Quiz/index.html" link>Code Quiz</h1>
+    <img src="link to image from in assets folder">a screenshot from the website that is uploaded to the asset folder</img>
+    <link href="https://github.com/Amdalat1000000/Code-Quiz" link>Code Quiz Github Repository</link>
+    </div>
+    <div>  
+    <h1 href="https://amdalat1000000.github.io/Work-Day-Scheduler/" link>Work Day Scheduler</h1>
+    <img src="link to image from in assets folder">a screenshot from the website that is uploaded to the asset folder</img>
+    <link href="https://github.com/Amdalat1000000/Work-Day-Scheduler" link>Work Day Scheduler Github Repository</link>
+    </div>
+    <div>  
+    <h1 href="https://amdalat1000000.github.io/Bootstrap-Portfolio/" link>Bootstrap Portfolio</h1>
+    <img src="link to image from in assets folder">a screenshot from the website that is uploaded to the asset folder</img>
+    <link href="https://github.com/Amdalat1000000/Bootstrap-Portfolio" link>Bootstrap Portfolio Github Repository</link>
+    </div>
+    <div>  
+    <h1 href="https://amdalat1000000.github.io/challenge2/" link>Portfolio</h1>
+    <img src="link to image from in assets folder">a screenshot from the website that is uploaded to the asset folder</img>
+    <link href="https://github.com/Amdalat1000000/challenge2" link>Portfolio Github Repository</link>
+    </div>
+    <div>  
+    <h1 href="https://amdalat1000000.github.io/RefactorHorizonAssignment/" link>Refactor Horizon</h1>
+    <img src="link to image from in assets folder">a screenshot from the website that is uploaded to the asset folder</img>
+    <link href="https://github.com/Amdalat1000000/RefactorHorizonAssignment" link>Refactor Horizon Github Repository</link>
+    </div>
+    {/* have 6 examples of websites and matching github repos*/ }
+    </div>
     )
 }
 
-export default project;
+export default Project;
