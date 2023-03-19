3f90076f19cc147933066f04b8e0cbdd9ae2a29a
Amdalat Jinadu <babi.jinadu@gmail.com>
2023-03-19 14:15:18 +0000

    amendmments made

---
+++ src/components/Project-Gallery.js
@@ -0,0 +1,15 @@
+import roject from "./components/project.js"
+//passing my projects to the projectgallery function you can use useState (contains the description of the projects) and then that use usestate object have all the names of all your rpoject and the descriptions and all the requirements or my project and then reurn it into your div project gallery would contain all the css to style the gallery to render it on-screen as a gallery
+//inside project gallery div I will be passing in my project
+function projectGallery() {
+    return (
+        <div id="projectGallery">
+            //pass the project into the gallery//
+            
+        <img></img>
+        <h1>Amdalat Jinadu</h1>
+        <p>paragraph discussing my skills and experience.</p>
+        </div>
+      );
+}
+export default home;
---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+project() {
+    return (
+    <div>
+    <h1 href=project.js link>project title</h1>
+    <img>from asset folder</img>
+    <link href=project,js link></link>
+    </div>
+    
+    )
+}
+
+export default project;
