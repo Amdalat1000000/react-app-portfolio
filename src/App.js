8be035c2c8c40eb180f19cece622d6b5840114b5
Amdalat Jinadu <babi.jinadu@gmail.com>
2023-03-20 04:00:51 +0000

    amendment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header.js";
 import Home from "./components/Home.js";
-import ProjectGallery from "./components/Project-Gallery";
+import ProjectGallery from "./components/Project-Gallery.js";
 
 
 function App() {
